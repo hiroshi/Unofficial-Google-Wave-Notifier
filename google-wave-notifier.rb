09cc89376476e9bc58f9a0253f0f050cd3911c41
Hiroshi Saito <hiroshi3110@gmail.com>
2009-10-18 17:05:14 +0900

    Initial commit

---
+++ google-wave-notifier.rb
@@ -0,0 +1,157 @@
+#!/usr/bin/env ruby
+
+# NOTE: Main part of this script is result of inspection on http://thatsmith.com/2009/10/google-wave-add-on-for-firefox 
+
+require "net/https"
+require "yaml"
+
+Net::HTTP.class_eval do
+  def self.ssl_new(uri)
+    http = Net::HTTP.new(uri.host, uri.port)
+    if uri.scheme == "https"  # enable SSL/TLS
+      http.use_ssl = true 
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    end
+    http
+  end
+end
+
+module GoogleWave
+  class Notifier
+    class InboxItem
+      def initialize(hash)
+        @hash = hash
+      end
+
+      def title
+        @hash["9"]["1"]
+      end
+
+      def unread_count
+        @hash["7"]
+      end
+
+      def inspect
+        "#<InboxItem: '#{title}' (#{unread_count})>"
+      end
+    end
+
+    class Inbox
+      def initialize(body)
+        json = body.match(/var json = (\{\s?"r"\s?:\s?"\^d1".*\});/)[1]
+        # tidy json to be capable for the YAML parser
+        yaml = json.gsub(/:([^ ])/){ ": #{$1}" }.gsub(/,/, ", ") # FIXME: can't deal with ':' and ',' in key or value
+        @hash = YAML.load(yaml)
+      end
+
+      def items
+        @items ||= @hash["p"]["1"].map{|e| InboxItem.new(e)}
+      end
+
+      def unread_items
+        self.items.select{|i| i.unread_count > 0}
+      end
+
+      def total_unread_count
+        self.unread_items.inject(0){|s,i| s += i.unread_count}
+      end
+    end
+
+    def self.get_inbox(email, password)
+      notifier = Notifier.new
+      unless ARGV[2]
+        notifier.login(email, password)
+      end
+      notifier.get_inbox
+    end
+
+    def login(email, password)
+      uri = URI.parse('https://www.google.com/accounts/ClientLogin')
+      http = Net::HTTP.ssl_new(uri)
+
+      header = {
+        'Content-Type' => 'application/x-www-form-urlencoded',
+      }
+      data = {
+        'accountType' => 'GOOGLE',
+        'Email' => email,
+        'Passwd' => password,
+        'service' => 'wave',
+        'source' => 'google-wave-notifier.rb',
+      }
+      http.start do
+        req = Net::HTTP::Post.new(uri.path, header)
+        req.form_data = data
+        case res = http.request(req)
+        when Net::HTTPSuccess
+          @login_info = res.body.split(/\n/).inject({}){|h,l| k,v = l.split("="); h.update(k => v)}
+        else # FIXME
+          
+        end
+      end
+    end
+
+    def get_inbox
+      unless ARGV[2]
+        uri = URI.parse('https://wave.google.com/wave/?nouacheck&auth=' + @login_info["Auth"])
+        http = Net::HTTP.ssl_new(uri)
+
+        response_body = http.start do
+          auth_res = http.request_get(uri.path + "?" + uri.query)
+          cookie = auth_res["set-cookie"]
+          redirect_uri = URI.parse(auth_res["location"])
+          
+          res = http.request_get(redirect_uri.path + "?" + redirect_uri.query, {"cookie" => cookie})
+          res.body
+        end
+      else
+        uri = URI.parse('https://wave.google.com/wave/?nouacheck')
+        http = Net::HTTP.ssl_new(uri)
+
+        response_body = http.start do
+          res = http.request_get(uri.path + "?" + uri.query, {"cookie" => ARGV[2]})
+          res.body
+        end
+      end
+      Inbox.new(response_body)
+    end
+  end
+end
+
+
+if $0 == __FILE__
+  email, password = ARGV
+  if !email || !password
+    puts "usage:\n  #{$0} email password"
+    exit 1
+  end
+
+  inbox = GoogleWave::Notifier.get_inbox(email, password)
+  #p inbox.items
+
+  # output unread items as a plist
+  items = inbox.unread_items.map do |item|
+    <<-ITEM
+      <dict>
+        <key>Title</key>
+        <string>#{item.title}</string>
+        <key>Unread Count</key>
+        <integer>#{item.unread_count}</integer>
+      </dict>
+    ITEM
+  end
+  puts <<-PLIST
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <dict>
+    <key>Total Unread Count</key>
+    <integer>#{inbox.total_unread_count}</integer>
+    <key>Items</key>
+    <array>
+#{items.join}
+    </array>
+  </dict>
+</plist>
+  PLIST
+end
---
+++ main.m
@@ -0,0 +1,199 @@
+//
+//  main.m
+//  google-wave-notifier
+//
+//  Created by hiroshi on 09/10/16.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+#import <Cocoa/Cocoa.h>
+#define SERVICE_NAME "Unofficial Google Wave Notifier" // FIXME: get from app
+
+@interface AppDelegate : NSObject
+{
+    IBOutlet id menu;
+    IBOutlet id preferencesWindow;
+    NSStatusItem *statusItem;
+}
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification;
+
+- (IBAction)checkNotificationAsync:(id)sender;
+- (void)checkNotification:(NSTimer*)theTimer;
+
+- (NSString *)password;
+- (void)setPassword:(NSString *)value;
+
+- (IBAction)openPreferences:(id)sender;
+
+//- (NSDictionary *)infoDictionary;
+@end
+
+@implementation AppDelegate
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength: NSVariableStatusItemLength];
+    [statusItem setTitle: @"w"];
+    [statusItem setHighlightMode: YES];
+    [statusItem setMenu: menu];
+
+    // check once first
+    [self checkNotificationAsync: self];
+    // schedule checkNotification
+    [NSTimer scheduledTimerWithTimeInterval: 1.0 * 60 // FIXME: 
+             target: self
+             selector: @selector(checkNotification:)
+             userInfo: nil
+             repeats: YES];
+}
+
+- (void)checkNotificationAsync:(id)sender
+{
+    [preferencesWindow performClose: sender];
+    [NSTimer scheduledTimerWithTimeInterval: 1.0
+             target: self
+             selector: @selector(checkNotification:)
+             userInfo: nil
+             repeats: NO];
+}
+
+- (void)checkNotification:(NSTimer*)theTimer
+{
+    // first of all, check keychain for password
+    NSString *email = [[NSUserDefaults standardUserDefaults] objectForKey: @"Email"];
+    if (!email)
+    {
+        [statusItem setTitle: @"w(x)"];
+        return;
+    }
+    NSString *password = [self password];
+    if (!password)
+    {
+        [statusItem setTitle: @"w(x)"];
+        return;
+    }
+    //NSLog(@"e: %@, p: %@\n", email, password);
+
+    NSLog(@"checkNotification: start...\n");
+    NSTask *task = [[NSTask alloc] init];
+    NSPipe *pipe = [NSPipe pipe];
+    NSFileHandle *handle = [pipe fileHandleForReading];
+    [task setLaunchPath: @"/usr/bin/ruby"];
+    NSString *rbPath = [[NSBundle mainBundle] pathForResource: @"google-wave-notifier" ofType: @"rb"];
+    //NSLog(@"rbPath: %@\n", rbPath);
+    [task setArguments: [NSArray arrayWithObjects: rbPath, email, password, nil]];
+    [task setStandardOutput: pipe];
+    [task launch];
+
+    NSData *data = [handle readDataToEndOfFile];
+    //NSLog([[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding]);
+    //NSLog(@"\n");
+    [task waitUntilExit];
+    if ([task terminationStatus] != 0)
+    {
+        [statusItem setTitle: @"w(x)"];
+        return;
+    }
+
+    NSString *errorString = nil;
+    NSPropertyListFormat format;
+    id plist = [NSPropertyListSerialization propertyListFromData: data 
+                                            mutabilityOption: NSPropertyListImmutable
+                                            format: &format
+                                            errorDescription: &errorString];
+    if (errorString)
+    {
+        NSLog(@"checkNotification: failed. error: %@\n", *errorString);
+    }
+    else
+    {
+        //id count = [[[plist objectForKey: @"Items"] objectAtIndex: 0] objectForKey: @"Unread Count"];
+        id count = [plist objectForKey: @"Total Unread Count"];
+        if ([count isEqualToNumber: [NSNumber numberWithInt: 0]])
+        {
+            [statusItem setTitle: @"w"];
+        }   
+        else
+        {
+            [statusItem setTitle: [NSString stringWithFormat: @"w(%@)", count]];
+        }
+        NSLog(@"checkNotification: done. (count: %@)\n", count);
+    }
+}
+
+- (NSString *)password
+{
+    NSString *email = [[NSUserDefaults standardUserDefaults] objectForKey: @"Email"];
+    if (email)
+    {
+        void *passwordData;
+        UInt32 passwordLength;
+        OSStatus status = SecKeychainFindGenericPassword(
+            NULL,           // default keychain
+            strlen(SERVICE_NAME),             // length of service name
+            SERVICE_NAME,   // service name
+            [email lengthOfBytesUsingEncoding: NSUTF8StringEncoding], // length of account name
+            [email UTF8String],   // account name
+            &passwordLength,  // length of password
+            &passwordData,   // pointer to password data
+            NULL // the item reference
+            );
+        if (status != noErr)
+        {
+            NSLog(@"SecKeychainFindGenericPassword: failed. (OSStatus: %d)\n", status); // FIXME: handle the errror
+            return nil;//@"";
+        }
+        //NSString *passwd = [NSString stringWithUTF8String: passwordData];
+        NSString *passwd = [[NSString alloc] initWithBytes: passwordData length: passwordLength encoding: NSUTF8StringEncoding];
+        status = SecKeychainItemFreeContent(NULL, passwordData);
+        if (status != noErr)
+        {
+            NSLog(@"SeSecKeychainItemFreeContent: failed. (OSStatus: %d)\n", status); // FIXME: handle the errror
+        }
+        return passwd;
+    }
+    else
+    {
+        return nil; //@"";
+    }
+}
+- (void)setPassword:(NSString *)value
+{
+    NSString *email = [[NSUserDefaults standardUserDefaults] objectForKey: @"Email"];
+    if (email)
+    {
+        OSStatus status = SecKeychainAddGenericPassword (
+            NULL,           // default keychain
+            strlen(SERVICE_NAME),             // length of service name
+            SERVICE_NAME,   // service name
+            [email lengthOfBytesUsingEncoding: NSUTF8StringEncoding], // length of account name
+            [email UTF8String],   // account name
+            [value lengthOfBytesUsingEncoding: NSUTF8StringEncoding], // length of password
+            [value UTF8String],   // password
+            NULL
+            );
+        if (status != noErr)
+        {
+            NSLog(@"SecKeychainAddGenericPassword: failed. (OSStatus: %d)\n", status); // FIXME: handle the errror
+        }
+    }
+}
+
+- (IBAction)openPreferences:(id)sender
+{
+    [preferencesWindow makeKeyAndOrderFront: sender];
+    [preferencesWindow setLevel: NSTornOffMenuWindowLevel];
+}
+
+// - (NSDictionary *)infoDictionary
+// {
+//     return [[NSBundle mainBundle] infoDictionary];
+// }
+@end
+
+
+
+
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
