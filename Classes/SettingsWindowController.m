5f0c3534c67f2044ae4bbe622aa1037726239b40
Hiroshi Saito <hiroshi3110@gmail.com>
2010-01-06 00:59:55 +0900

    Fix a case of a path to browser image is nil.
    This may happen with MobileSafari.app of iPhone SDK (Don't have an app icon).

--- Classes/SettingsWindowController.m
+++ Classes/SettingsWindowController.m
@@ -32,7 +32,6 @@ NSString *WaveClientBundleIDKey = @"WaveClientBundleID";
     NSWorkspace *ws = [NSWorkspace sharedWorkspace];
 	NSString *storedBundleId =  [[NSUserDefaults standardUserDefaults] objectForKey:WaveClientBundleIDKey];
     
-	NSImage *browserImage = nil;
 	NSString *browserAppName = nil;
 	NSString *appPath = nil;
 	NSBundle *bundle = nil;
@@ -41,19 +40,25 @@ NSString *WaveClientBundleIDKey = @"WaveClientBundleID";
 	
 	for(NSString *bundleId in appBundleIds)
 	{
+        NSImage *browserImage = nil;
 		appPath = [ws absolutePathForAppBundleWithIdentifier: bundleId];
-		bundle = [NSBundle bundleWithPath:appPath];
-        
-		browserImage = [ws iconForFile:appPath];
-		browserAppName = [[bundle infoDictionary] objectForKey:(NSString *)kCFBundleNameKey];
+        if (appPath)
+        {
+            bundle = [NSBundle bundleWithPath:appPath];
+            browserImage = [ws iconForFile:appPath];
+        }
+        browserAppName = [[bundle infoDictionary] objectForKey:(NSString *)kCFBundleNameKey];
 		
-		if(!browserAppName)
+		if(!browserAppName) 
 			browserAppName = [appPath lastPathComponent];
         
 		NSMenuItem *menuItem = [[[NSMenuItem alloc] initWithTitle:browserAppName 
                                                            action:@selector(saveSelection:) 
                                                     keyEquivalent:@""] autorelease];
-		[menuItem setImage:browserImage];
+        if (browserImage)
+        {
+            [menuItem setImage:browserImage];
+        }
 		[popupMenu addItem:menuItem];
 		
 		if ([storedBundleId isEqual: bundleId]) {
